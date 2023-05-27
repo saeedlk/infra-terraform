9a18afbeab441b7b543ddb0414662108863fb916
Saeid <98920239+saeedlk@users.noreply.github.com>
2023-05-27 11:40:41 +0330

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -33,4 +33,27 @@ resource "aws_security_group" "http_8080" {
   }
 }
 
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
 
+  name = "basic-webapp"
+
+  ami           = data.hcp_packer_image.amazon_linux.cloud_image_id
+  instance_type = "t2.micro"
+  # user_data     = file("${path.module}/userdata.sh")
+
+  vpc_security_group_ids = [aws_security_group.http_8080.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+output "health_endpoint" {
+  description = "URL of the health check endpoint"
+  value = "http://${module.ec2_instance.public_ip}:8080/healthz"
+}
