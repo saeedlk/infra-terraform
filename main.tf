d196410b28563846a04dd7a4ef5586a774156d6b
Saeidlk <slk@romoels-macbook-air.local>
2023-06-04 18:47:06 +0330

    update main to provision remote-ecex

--- main.tf
+++ main.tf
@@ -24,22 +24,24 @@ resource "aws_instance" "ec2_instance_lk" {
   # ami           = "ami-0b08bfc6ff7069aff"
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
-
-#   vpc_security_group_ids = [aws_security_group.launch_wizard_lk.id]
+  key_name = "singapore-ssh"
+  vpc_security_group_ids = [aws_security_group.sec_lk.id]
   
-  connection {
-    type = "ssh"
-    user = "ec2-user"
-    password = ""
-    host = self.public_ip
-  }
 
   provisioner "remote-exec" {
     inline = [
+      "echo ${self.private_ip} >> /home/ec2-user/private_ips.txt",
       "mkdir /home/ec2-user/dir1",
       "cd ./home/ec2-user/dir1",
       "mkdir dir2"
     ]
+  }
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = "${self.public_ip}"
+    private_key = "${file("/Users/slk/Downloads/singapore")}"
   }
 
   tags = {
